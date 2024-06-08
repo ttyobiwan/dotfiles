040e13ddf8cee6cde0df56f19cddd340f12212fb
tobiwandev <piotr.tobiasz.dev@gmail.com>
2024-06-08 12:41:18 +0200

    Fix this and that

--- .config/nvim/lua/tobi/init.lua
+++ .config/nvim/lua/tobi/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,6 +22,20 @@ vim.o.shiftwidth = 4
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('n', '<C-S>', ':w<CR>', { noremap = true, silent = true })
+
+vim.keymap.set(
+	"n",
+	"<leader>zz",
+	function()
+		local fcvalue = vim.api.nvim_win_get_option(0, "foldcolumn")
+		if fcvalue == "0" then
+			vim.cmd("set foldcolumn=9")
+		else
+			vim.cmd("set foldcolumn=0")
+		end
+	end,
+	{ desc = "Add left margin", remap = true }
+)
 
 vim.keymap.set("n", "<S-h>", ":bprevious<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<S-l>", ":bnext<cr>", { desc = "Next buffer" })
--- .config/nvim/lua/tobi/plugins/lualine.lua
+++ .config/nvim/lua/tobi/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			options = {
+				globalstatus = true,
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
 			},
